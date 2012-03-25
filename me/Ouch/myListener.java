46c0bc1cc108bb7ce0be4151d559b1d041235d50
Steven Smith <stevensmith.ome@gmail.com>
2012-03-26 00:14:55 +0800

    comm

---
+++ me/Ouch/myListener.java
@@ -0,0 +1,20 @@
+package me.Ouch;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class myListener implements Listener{
+	
+	@EventHandler
+	public void blockBreak(BlockBreakEvent event){
+		Player p = event.getPlayer();
+		p.sendMessage("Ouch.");
+	}
+	public void blockPlace(BlockPlaceEvent event){
+		Player p = event.getPlayer();
+		p.sendMessage("Ahhhhh.....");
+	}
+}
