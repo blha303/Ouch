8ef824845801794ba9c6153a22095676bf54d7ca
Steven Smith <stevensmith.ome@gmail.com>
2012-03-27 10:54:15 +0800

    Update cc/co/blha303/Ouch/myListener.java

--- cc/co/blha303/Ouch/myListener.java
+++ cc/co/blha303/Ouch/myListener.java
@@ -5,25 +5,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.plugin.java.JavaPlugin;
-import cc.co.blha303.Ouch;
 
 public class myListener implements Listener{
-	
+
 	@EventHandler
 	public void blockBreak(BlockBreakEvent event){
 		Player p = event.getPlayer();
 		p.sendMessage("Ouch.");
-		plugin.getServer().getConsoleSender().sendMessage("Ouch.");
 	}
 	@EventHandler
 	public void blockPlace(BlockPlaceEvent event){
 		Player p = event.getPlayer();
 		p.sendMessage("Ahhhhh.....");
-		plugin.getServer().getConsoleSender().sendMessage("Ahhhhh.....");
-	}
-	private Ouch plugin; 
-	public myListener(Ouch instance) { 
-		this.plugin = instance; 
 	}
 }
