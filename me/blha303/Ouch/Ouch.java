bc40062f37d592c82c44e576970e58fad6a2a676
Steven Smith <stevensmith.ome@gmail.com>
2012-03-30 04:21:38 +0800

    changed namespace again

---
+++ me/blha303/Ouch/Ouch.java
@@ -0,0 +1,26 @@
+package me.blha303.Ouch;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Ouch extends JavaPlugin {
+	
+	public static Ouch plugin;
+	public final Logger logger = Logger.getLogger("Minecraft");
+	
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		this.logger.info(this.getDescription().getName() + " is now disabled.");
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		getServer().getPluginManager().registerEvents(new myListener(), this);
+		this.logger.info("[" + this.getDescription().getName() + "]" + " Enabled.");
+	}
+
+}
