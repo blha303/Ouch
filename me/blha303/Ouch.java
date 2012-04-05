7d98664e2c46a1e45e3aba499f66227c9a5c0832
Steven Smith <stevensmith.ome@gmail.com>
2012-04-05 10:18:11 +0800

    ouch

--- me/blha303/Ouch.java
+++ me/blha303/Ouch.java
@@ -1,16 +1,9 @@
 package me.blha303;
 
-import java.io.File;
 import java.util.logging.Logger;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Ouch extends JavaPlugin {
-	
-	
 	
 	public static Ouch plugin;
 	public final Logger logger = Logger.getLogger("Minecraft");
@@ -23,15 +16,9 @@ public class Ouch extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new myListener(), this);
+		this.getConfig().options().copyDefaults(true);
+		saveConfig();
 		this.logger.info("[" + this.getDescription().getName() + "]" + " " + getConfig().getString("lang.enable"));
-	}
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		if(cmd.getName().equalsIgnoreCase("ouch")){
-				reloadConfig();
-				this.logger.info("Ouch config reloaded.");
-			return true;
-		}
-		return false; 
 	}
 
 }
